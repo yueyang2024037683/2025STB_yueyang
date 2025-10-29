8ebe08489eb2eecfebc37ade456a88eba1f95d59
악양 <yy2024037683@hanyang.ac.kr>
2025-10-29 21:07:31 +0900

    update

--- 09. sampling distribution.R
+++ 09. sampling distribution.R
@@ -17,3 +17,35 @@ print(meanofz)
 data50=c(1:50)#데이터
 com2=combn(data50,2)#데이터에서 2개씩 선택하여 데이터 생성
 com2t=t(com2)#데이터의 전치
+z2=rowMeans(com2t)#전치된 데이터 행평균 계산
+meanofz2=mean(z2)#기댓값을 계산해 저정
+print(meanofz2)
+barplot(table(z2),xlab="평균2",ylab="도수",main="표본크기2의 도수분포표")
+
+#sample size=3
+com3=combn(data50,3)#데이터에서 3개씩 선택하여 데이터 생성
+com3t=t(com3)#데이터의 전치
+z3=rowMeans(com3t)#전치된 데이터 행평균 계산
+meanofz3=mean(z3)##기댓값을 계산해 저정
+print(meanofz3)
+barplot(table(z3),xlab="평균3",ylab="도수",main="표본크기3의 도수분포표")
+
+#sample size=5
+com5=combn(data50,5)#데이터에서 5개씩 선택하여 데이터 생성
+com5t=t(com5)#데이터의 전치
+z5=rowMeans(com5t)#전치된 데이터 행평균 계산
+meanofz5=mean(z5)##기댓값을 계산해 저정
+print(meanofz5)
+barplot(table(z5),xlab="평균5",ylab="도수",main="표본크기5의 도수분포표")
+
+#표본추출방법: 단순무작위추출법
+sampling=c(10:100)
+print(sampling)
+sample(x=sampling,size=10)
+
+#표본추출방법: 층화추출법
+install.packages("sampling")
+library(sampling)
+sam2=strata(data=iris,stratanames=c("Species"),size=c(3,3,3),method='srswor')#iris데에터 사용하여 표본추출,Species층을 기준으로
+sample2=getdata(data=iris,m=sam2)#추출된 데이터 저장
+print(sample2)
